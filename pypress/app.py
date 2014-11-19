77a69d0431a9741f1ea81c81a414301b71862d20
Elvin Yung <elvinyung1@gmail.com>
2014-11-18 23:49:13 -0500

    moved app methods into app

--- pypress/app.py
+++ pypress/app.py
@@ -3,16 +3,19 @@ from wsgiref.simple_server import make_server
 router = {}
 
 class Application():
-	def __init__(self):
-		print 5
-
-def route(fn):
-	def wrapper(*args, **kwargs):
+	def _app(environ, start_response):
 		pass
+
+	def __init__(self):
+		self.app = _app
+
+	def route(fn):
+		def wrapper(*args, **kwargs):
+			pass
 	return wrapper
 
-def listen(port=1337, hostname=''):
-	server_class = BaseHTTPServer.BaseHTTPServer
-	request_handler_class = BaseHTTPServer.BaseHTTPRequestHandler
-	server = server_class((hostname, port), request_handler_class)
-	server.serve_forever()
+	
+	def listen(port=1337, hostname=''):
+		server = make_server(hostname, port, self.app)
+		server.serve_forever()
+
