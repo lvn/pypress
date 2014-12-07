bc124fccd6a27a97f5339d66f36afae3b9bcfe26
Elvin Yung <elvinyung1@gmail.com>
2014-12-07 01:42:53 -0500

    updated hello world example for PEP8 stuff

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from pypress import Pypress
 
+# A basic Pypress app that displays Hello World.
+
 app = Pypress()
 
+
 @app.get('/')
-def hello (req, res):
+def hello(req, res):
     res.send('Hello world!')
 
-app.listen(1337) 
+app.listen(1337)
