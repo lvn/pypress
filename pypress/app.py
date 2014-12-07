2c1c7e623739fd4ffa09af4807acdb8142bb9543
Elvin Yung <elvinyung1@gmail.com>
2014-12-07 04:49:00 -0500

    added support for ALL HTTP METHODS

--- pypress/app.py
+++ pypress/app.py
@@ -2,6 +2,7 @@ import http.server
 from .request import Request
 from .response import Response
 from .router import Router
+from .methods import methods
 
 
 class PypressServer(http.server.HTTPServer):
@@ -11,18 +12,13 @@ class PypressServer(http.server.HTTPServer):
 class PypressRequestHandler(http.server.BaseHTTPRequestHandler):
     routes = {}
 
+    def construct_middleware_stack(self, route, method):
+        pass
+
     def handle_request(self):
-        request_obj = Request()
-        response_obj = Response()
-
-        print(str(self.headers))
-        print(str(self.path))
-
-    do_GET = handle_request
-    do_POST = handle_request
-    do_PUT = handle_request
-    do_PATCH = handle_request
-    do_DELETE = handle_request
+        req = Request(self)
+        res = Response(self)
+        middleware_stack = construct_middleware_stack(self.path, self.command)
 
 
 class Application():
@@ -30,13 +26,14 @@ class Application():
     routes = {}
 
     def __init__(self):
-        pass
+        # generates a route adder for every HTTP method.
+        for method in methods:
+            self._generate_add_route_method(method)
 
     # helper method for adding routes.
     # if middleware is provided, mount the middleware at the path.
     # if not, return a decorator that mounts the function as middleware at path.
     def _add_route(self, path, middleware=None, method=None):
-
         if path not in self.routes:
             self.routes[path] = {} if method else []
 
@@ -54,29 +51,20 @@ class Application():
         else:
             return add_route
 
+    # generates an add_route method for EVERY method.
+    # this is for app.get, app.post, app.trace, app.mkactivity, etc.
+    def _generate_add_route_method(self, method):
+        add_route = self._add_route
+        def add_route_method(self, path='*', *middleware):
+            return add_route(path, middleware, method)
+
+        # temporary solution ?? ? ? ?
+        setattr(self, method, add_route_method)
+        return add_route_method
+
     # mount the middleware for all requests to the path.
     def use(self, path='*', *middleware):
         return self._add_route(path, middleware)
-
-    # mount the middleware for all GET requests to the path.
-    def get(self, path='*', *middleware):
-        return self._add_route(path, middleware, 'GET')
-
-    # mount the middleware for all POST requests to the path.
-    def post(self, path='*', *middleware):
-        return self._add_route(path, middleware, 'POST')
-
-    # mount the middleware for all PUT requests to the path.
-    def put(self, path='*', *middleware):
-        return self._add_route(path, middleware, 'PUT')
-
-    # mount the middleware for all GET requests to the path.
-    def patch(self, path='*', *middleware):
-        return self._add_route(path, middleware, 'PATCH')
-
-    # mount the middleware for all DELETE requests to the path.
-    def delete(self, path='*', *middleware):
-        return self._add_route(path, middleware, 'DELETE')
 
     def route(self, path='*'):
         pass
@@ -84,5 +72,9 @@ class Application():
     def listen(self, port=1337, hostname='', backlog=511, callback=None):
         server_address = ('', 1337)
         PypressRequestHandler.routes = self.routes
+        for method in methods:
+            setattr(PypressRequestHandler,
+                    'do_{0}'.format(method.upper()),
+                    PypressRequestHandler.handle_request)
         server = PypressServer(server_address, PypressRequestHandler)
         server.serve_forever()
---
+++ pypress/methods.py
@@ -0,0 +1,6 @@
+
+# ALL http methods.
+methods = ["get", "post", "put", "head", "delete", "options", "trace"] + \
+    ["copy", "lock", "mkcol", "move", "purge", "propfind", "proppatch"] + \
+    ["unlock", "report", "mkactivity", "checkout", "merge", "m-search"] + \
+    ["notify", "subscribe", "unsubscribe", "patch", "search", "connect"]
