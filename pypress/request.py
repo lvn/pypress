53e822441eaf0302ff600a8f43b614f2698b5db9
Elvin Yung <elvinyung1@gmail.com>
2014-12-10 22:39:18 -0500

    added header parsing to request

--- pypress/request.py
+++ pypress/request.py
@@ -1,8 +1,14 @@
 
+def _header_to_dict(header_message):
+    header_message = header_message.as_string()
+    headers = [tuple(h.split(': ', 1)) for h in header_message.split('\n') if h]
+    return {key: val for key, val in headers}
+
 
 class Request():
     def __init__(self, request_handler):
         self.request_handler = request_handler
+        self.headers = _header_to_dict(self.request_handler.headers)
         self.params = self.request_handler.params
         self.query = self.request_handler.query
         if 'Content-Length' in self.request_handler.headers:
